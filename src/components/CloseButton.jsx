54d38e9ce3f5fc8371480b7e55ac0d5f68cf91c2
klee616 <145302582+klee616@users.noreply.github.com>
2024-09-28 19:15:57 -0700

    fixed bug

--- src/components/CloseButton.jsx
+++ src/components/CloseButton.jsx
@@ -1,9 +1,28 @@
+
+import React, { useRef, useEffect } from 'react';
+
 export default function CloseButton({ }) {
-function
+
+    const myRef = useRef(null);
+
+    useEffect(() => {
+        myRef.current.addEventListener('click', handleClick);
+        return () => {
+            myRef.current.removeEventListener('click', handleClick);
+        };
+    }, []);
+
+    const handleClick = (event) => {
+        let parentEle =   event.target.parentNode;
+        do {
+            parentEle = parentEle.parentNode;
+        } while (parentEle.className != "story-block" || parentEle == null)
+        parentEle.style.display = "none";
+    };
 
     return (<>
-        <span>
-            <button>X</button>
+        <span className="span">
+            <button ref={myRef}>X</button>
         </span>
     </>)
 }
